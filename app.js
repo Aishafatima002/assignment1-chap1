e5b8f53f467bd7b55e3a18fa5ee47b6ef6a6fe85
Aishafatima002 <fatimaaisha002@gmail.com>
2024-05-25 23:56:41 -0700

    new

---
+++ app.js
@@ -0,0 +1,15 @@
+//Q1
+// let visitors="Error please enter a valid password";
+// alert(visitors)
+
+//Q2
+// let text="welcome to js land\n happy coding"
+// alert(text)
+
+//Q3
+// let text="welcome to jsland"
+// alert(text)
+// let text1="Happy coding"
+// alert(text1)
+
+
